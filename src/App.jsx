9f5112292497ba0e906b905cea18e4d4f0dd1b47
Ayush Mittal <mittalayush806@gmail.com>
2026-07-21 11:28:01 +0530

    Providing Protected route

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,26 @@ import Dashboard from './pages/Dashboard.jsx'
 import FlightDetails from './pages/FlightDetails.jsx'
 import SearchResults from './pages/SearchResults.jsx'
 import Bookings from './pages/Bookings.jsx'
+import SavedFlights from './pages/SavedFlights.jsx'
+import Wishlist from './pages/Wishlist.jsx'
+import TravelWallet from './pages/TravelWallet.jsx'
 import Offers from './pages/Offers.jsx'
 import Support from './pages/Support.jsx'
 import ExpenseReport from './pages/ExpenseReport.jsx'
 import AddExpense from './pages/AddExpense.jsx'
 import TripReport from './pages/TripReport.jsx'
 import ExpenseAnalytics from './pages/ExpenseAnalytics.jsx'
+import Settings from './pages/Settings.jsx'
 import DashboardLayout from './layouts/DashboardLayout.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+
+function Protected({ page }) {
+  return (
+    <ProtectedRoute>
+      <DashboardLayout>{page}</DashboardLayout>
+    </ProtectedRoute>
+  )
+}
 
 export default function App() {
   return (
@@ -27,102 +40,20 @@ export default function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
 
         {/* Protected Dashboard Pages */}
-        <Route
-          path="/dashboard"
-          element={
-            <DashboardLayout>
-              <Dashboard />
-            </DashboardLayout>
-          }
-        />
-
-        {/* Flight Details */}
-        <Route
-          path="/flights/:id"
-          element={
-            <DashboardLayout>
-              <FlightDetails />
-            </DashboardLayout>
-          }
-        />
-
-        {/* Search Results */}
-        <Route
-          path="/search-results"
-          element={
-            <DashboardLayout>
-              <SearchResults />
-            </DashboardLayout>
-          }
-        />
-
-        {/* Expense Module */}
-        <Route
-          path="/expenses"
-          element={
-            <DashboardLayout>
-              <ExpenseReport />
-            </DashboardLayout>
-          }
-        />
-        <Route
-          path="/expenses/new"
-          element={
-            <DashboardLayout>
-              <AddExpense />
-            </DashboardLayout>
-          }
-        />
-
-        {/* My Trips (Trip Report) */}
-        <Route
-          path="/my-trips"
-          element={
-            <DashboardLayout>
-              <TripReport />
-            </DashboardLayout>
-          }
-        />
-
-        {/* Expense Analytics */}
-        <Route
-          path="/analytics"
-          element={
-            <DashboardLayout>
-              <ExpenseAnalytics />
-            </DashboardLayout>
-          }
-        />
-
-        {/* Bookings */}
-        <Route
-          path="/bookings"
-          element={
-            <DashboardLayout>
-              <Bookings />
-            </DashboardLayout>
-          }
-        />
-
-        {/* Offers */}
-        <Route
-          path="/offers"
-          element={
-            <DashboardLayout>
-              <Offers />
-            </DashboardLayout>
-          }
-        />
-
-        {/* Support */}
-        <Route
-          path="/support"
-          element={
-            <DashboardLayout>
-              <Support />
-            </DashboardLayout>
-          }
-        />
+         <Route path="/dashboard" element={<Protected page={<Dashboard />} />} />
+        <Route path="/flights/:id" element={<Protected page={<FlightDetails />} />} />
+        <Route path="/search-results" element={<Protected page={<SearchResults />} />} />
+        <Route path="/expenses" element={<Protected page={<ExpenseReport />} />} />
+        <Route path="/expenses/new" element={<Protected page={<AddExpense />} />} />
+        <Route path="/my-trips" element={<Protected page={<TripReport />} />} />
+        <Route path="/analytics" element={<Protected page={<ExpenseAnalytics />} />} />
+        <Route path="/settings" element={<Protected page={<Settings />} />} />
+        <Route path="/bookings" element={<Protected page={<Bookings />} />} />
+        <Route path="/saved-flights" element={<Protected page={<SavedFlights />} />} />
+        <Route path="/wishlist" element={<Protected page={<Wishlist />} />} />
+        <Route path="/wallet" element={<Protected page={<TravelWallet />} />} />
+        <Route path="/offers" element={<Protected page={<Offers />} />} />
+        <Route path="/support" element={<Protected page={<Support />} />} />
 
         {/* Catch-all fallback redirect */}
         <Route path="*" element={<Navigate to="/signin" replace />} />
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 import { ExpenseProvider } from './hooks/useExpenses.jsx'
 import { AdvancesProvider } from './hooks/useAdvances.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ExpenseProvider>
-      <AdvancesProvider>
-        <App />
-      </AdvancesProvider>
-    </ExpenseProvider>
+    <AuthProvider>
+      <ExpenseProvider>
+        <AdvancesProvider>
+          <App />
+        </AdvancesProvider>
+      </ExpenseProvider>
+    </AuthProvider>
   </StrictMode>,
 )
